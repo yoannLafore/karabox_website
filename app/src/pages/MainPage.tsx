723965d3a67fa1237664a32529e957817a5874ca
Yoann Lafore <yoann.lafore@gmail.com>
2024-11-10 01:29:53 +0100

    feat(mainpage): add placeholders for pages

---
+++ app/src/pages/MainPage.tsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header';
+
+function MainPage() {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <h1>Home</h1>
+        <p>Welcome to the Home page!</p>
+      </main>
+    </div>
+  );
+}
+
+export default MainPage;
