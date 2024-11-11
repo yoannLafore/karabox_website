a2e859884ec9d02aa7158831c22e3b51f8374f09
Yoann Lafore <yoann.lafore@gmail.com>
2024-11-11 23:01:38 +0100

    fix(mainpage): make main fill the main space

--- app/src/pages/MainPage.tsx
+++ app/src/pages/MainPage.tsx
@@ -1,13 +1,17 @@
+import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
+import './MainPage.scss';
 
 function MainPage() {
   return (
-    <div>
+    <div className="mainPageContainer">
       <Header />
       <main>
         <h1>Home</h1>
         <p>Welcome to the Home page!</p>
       </main>
+
+      <Footer />
     </div>
   );
 }
