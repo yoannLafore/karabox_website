5e0c8f11d65cc60ed4eca005a0d81bb35e4b6c36
Yoann Lafore <yoann.lafore@gmail.com>
2024-11-13 02:31:16 +0100

    feat(slot-selector): display variations of time slots for dev

--- app/src/components/slot_reservation/SlotSelector.tsx
+++ app/src/components/slot_reservation/SlotSelector.tsx
@@ -1,9 +1,37 @@
 import styles from './SlotSelector.module.scss';
+import TimeSlot, { TimeSlotStatus } from './time_slot/TimeSlot';
+
+function onClick(status: TimeSlotStatus) {
+  console.log(status);
+}
 
 function SlotSelector() {
   return (
     <div className={styles['slot-selector-container']}>
-      <h1>Slot Selector</h1>
+      <TimeSlot
+        hour={10}
+        minute={0}
+        status={TimeSlotStatus.PAST}
+        onClick={onClick}
+      />
+      <TimeSlot
+        hour={10}
+        minute={30}
+        status={TimeSlotStatus.RESERVED}
+        onClick={onClick}
+      />
+      <TimeSlot
+        hour={11}
+        minute={0}
+        status={TimeSlotStatus.AVAILABLE}
+        onClick={onClick}
+      />
+      <TimeSlot
+        hour={11}
+        minute={30}
+        status={TimeSlotStatus.SELECTED}
+        onClick={onClick}
+      />
     </div>
   );
 }
