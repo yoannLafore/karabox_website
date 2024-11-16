08f62e1e27f49e92f7298121e69620dd431b23c5
Yoann Lafore <yoann.lafore@gmail.com>
2024-11-16 03:01:03 +0100

    feat(description): add index scss

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import News from './pages/News';
+import './index.scss';
 
 function App() {
   return (
