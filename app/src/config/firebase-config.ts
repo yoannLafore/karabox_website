eabf1648b4a952daba66ddf254c718995315ea00
Yoann Lafore <yoann.lafore@gmail.com>
2024-11-17 16:44:27 +0100

    feat(firebase): initialize firebase

---
+++ app/src/config/firebase-config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyD23YY9BNB-xXN5Ys1FUwJ0oJecOXxsM2Q',
+  authDomain: 'karabox-e6b2f.firebaseapp.com',
+  projectId: 'karabox-e6b2f',
+  storageBucket: 'karabox-e6b2f.firebasestorage.app',
+  messagingSenderId: '768225763927',
+  appId: '1:768225763927:web:cd485bd0d9f2af264bf912',
+  measurementId: 'G-240E71JRMW',
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.tsx';
+import './config/firebase-config.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
