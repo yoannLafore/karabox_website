ccd053ef10890454d315de29e6c2551ad7e90798
Yoann Lafore <yoann.lafore@gmail.com>
2024-11-29 23:51:50 +0100

    feat(booking): add api services to book and unbook slots

--- app/src/services/bookingService.ts
+++ app/src/services/bookingService.ts
@@ -2,8 +2,11 @@ import { Dayjs } from 'dayjs';
 import { apiUrl } from '../config/apiConfig';
 import axios from 'axios';
 import { SlotBook } from '../models/api/bookings/slotBook';
+import { BookSlotResult } from '../models/api/bookings/bookSlotResult';
 
 const GET_BOOKINGS = '/get-booked-slots';
+const BOOK_SLOT = '/book-slot';
+const UNBOOK_SLOT = '/unbook-slot';
 
 async function getBookings(day: Dayjs, token: string | null) {
   const dayStr = day.format('YYYY-MM-DD');
@@ -28,4 +31,45 @@ async function getBookings(day: Dayjs, token: string | null) {
   return bookedSlots;
 }
 
-export { getBookings };
+async function bookSlot(
+  day: Dayjs,
+  hours: number,
+  minutes: number,
+  token: string,
+) {
+  const dayStr = day.format('YYYY-MM-DD');
+
+  const data = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    params: {
+      day: dayStr,
+      hours: hours,
+      minutes: minutes,
+    },
+  };
+
+  const res = await axios.get(`${apiUrl}${BOOK_SLOT}`, data);
+
+  const bookResult = BookSlotResult.fromApiResponse(res.data);
+
+  return bookResult;
+}
+
+async function unbookSlot(slotId: string, token: string) {
+  const data = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    params: {
+      slotId: slotId,
+    },
+  };
+
+  const res = await axios.get(`${apiUrl}${UNBOOK_SLOT}`, data);
+
+  return res.data.success;
+}
+
+export { getBookings, bookSlot, unbookSlot };
