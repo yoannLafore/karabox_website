949ff2806db1cc45058451588b1a5d52b061f256
Yoann Lafore <yoann.lafore@gmail.com>
2024-12-04 16:19:02 +0100

    feat(booking): add query logic to main page

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,11 +4,16 @@ import './index.scss';
 import App from './App.tsx';
 import './config/firebaseConfig.ts';
 import { AuthProvider } from './context/AuthContext.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
--- app/src/pages/MainPage.tsx
+++ app/src/pages/MainPage.tsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import Description from '../components/description/Description';
 import SlotBooking from '../components/slot_reservation/SlotBooking';
 import { SlotSelectorUI } from '../models/ui/slot_reservation/SlotSelectorUI';
 import styles from './MainPage.module.scss';
+import { useQuery } from '@tanstack/react-query';
+import dayjs, { Dayjs } from 'dayjs';
+import { getBookings } from '../services/bookingService';
 
 function MainPage() {
+  const [selectedDay, setSelectedDay] = useState<Dayjs>(dayjs());
+
+  const query = useQuery({
+    queryKey: ['bookedSlots', selectedDay],
+    queryFn: () => getBookings(selectedDay, null),
+  });
+
   return (
     <div className={styles['main-page-container']}>
       <main>
@@ -22,8 +33,15 @@ function MainPage() {
           <div className={styles['vertical-line']}></div>
           <div className={styles['slot-selector-container']}>
             <SlotBooking
-              timeSlotSelectorUI={new SlotSelectorUI([])}
-              onDayChange={() => {}}
+              timeSlotSelectorUI={SlotSelectorUI.fromSlotBooksApi(
+                query.data ? query.data : [],
+                new Date(),
+              )}
+              onDayChange={(day) => {
+                setSelectedDay(day);
+                // Refresh the query
+                //query.refetch();
+              }}
               onBookSlotClick={() => {}}
               onTimeSlotClick={() => {}}
             />
